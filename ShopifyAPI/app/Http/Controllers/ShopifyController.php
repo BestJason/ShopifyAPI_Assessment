717a23da75c50b0df9d2631a77b917c76ab74279
Jason <mrjasonedu@gmail.com>
2018-08-08 00:51:08 +1000

    Add some comments for ShopifyController

--- ShopifyAPI/app/Http/Controllers/ShopifyController.php
+++ ShopifyAPI/app/Http/Controllers/ShopifyController.php
@@ -7,31 +7,51 @@ use App\Models\ShopifyAPI;
 
 class ShopifyController extends Controller
 {
+
+  /**
+   * Create Webhook for customers create
+   *
+   * @return array
+   */
   public function createWebhookForCustomersCreate()
   {
     try {
+
+      // default code value
       $code = -1;
+
+      // get api object of shopify app
       $shopifyApi = new ShopifyAPI();
       if (empty($shopifyApi)) {
         $msg = 'Initializing API failed';
         throw new \Exception($msg);
       }
+
+      // Need to config webhook first
       $webhooks = config('shopify-app.webhooks');
       if (empty($webhooks)) {
           $msg = 'The webhooks is missing, Please set up in config/shopify-app.php first';
           throw new \Exception($msg);
       }
+
+      // get webhook topic from .env file
       $topic = env('SHOPIFY_WEBHOOK_CUSTOMERS_CREATE_TOPIC');
       if (empty($topic)) {
         $msg = 'The topic is missing';
         throw new \Exception($msg);
       }
+
+      // get webhook address from .env file
       $address = env('SHOPIFY_WEBHOOK_CUSTOMERS_CREATE_ADDRESS');
       if (empty($address)) {
         $msg = 'The address is missing';
         throw new \Exception($msg);
       }
+
+      // install webhook
       $result = $shopifyApi->installWebhook($topic, $address);
+
+      // return standard API data format
       $code = 1;
       $msg = 'ok';
       $response = [
